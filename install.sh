85fe688f29b19a05b99c390a32023de814b51a84
Or Kwitzel <orkv1234@gmail.com>
2026-03-22 16:05:39 +0200

    fix: use sudo in install script when needed

--- install.sh
+++ install.sh
@@ -5,5 +5,12 @@ OS=$(uname -s | tr '[:upper:]' '[:lower:]')
 ARCH=$(uname -m | sed 's/x86_64/amd64/;s/aarch64/arm64/')
 URL="https://github.com/TheDokT0r/tracer/releases/latest/download/tracer-${OS}-${ARCH}.tar.gz"
 
-curl -fsSL "$URL" | tar -xz -C /usr/local/bin
-echo "tracer installed to /usr/local/bin/tracer"
+INSTALL_DIR="/usr/local/bin"
+
+if [ -w "$INSTALL_DIR" ]; then
+  curl -fsSL "$URL" | tar -xz -C "$INSTALL_DIR"
+else
+  curl -fsSL "$URL" | sudo tar -xz -C "$INSTALL_DIR"
+fi
+
+echo "tracer installed to ${INSTALL_DIR}/tracer"
